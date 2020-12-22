11c6ae11f898f6063af1535d252cf8d71f12a8f5
Alexander <zaporozhskih.sasha@yandex.ru>
2020-12-22 08:34:12 +0300

    fix compareurl

--- 3.2.1/src/Main.java
+++ 3.2.1/src/Main.java
@@ -32,19 +32,24 @@ public class Main {
 
         return difstr;
     }
-    public static String CompareSimbolsURL(URL url1,URL url2) throws Exception{
+    public static String CompareSimbolsURL(URL url1,URL url2) throws IOException{
+
         BufferedReader readu1 = new BufferedReader(new InputStreamReader(url1.openStream()));
         BufferedReader readu2 = new BufferedReader(new InputStreamReader(url2.openStream()));
+
         String difstr="";
+        boolean dif=false;
         try {
             char a1;
             char a2;
+
             a1=(char)readu1.read();
             a2=(char)readu2.read();
             int i=0;
             while((i!=255)){
                 if(a1!=a2){
                     difstr+="//"+(i+1)+": ("+a1+", "+a2+")";
+                    dif=true;
                 }
                 a1=(char)readu1.read();
                 a2=(char)readu2.read();
@@ -54,18 +59,24 @@ public class Main {
             readu2.close();
         }catch (Exception e){
             e.getStackTrace();
+        }finally{
+                readu1.close();
+                readu2.close();
+        }
+        if(!dif){
+            difstr="Sites do not have a difference!";
         }
         return difstr;
-    } //dont work
+    } //dont work for https
     public static void main(String[] args) {
         try{
             File file1 = new File("a.txt");
             File file2 = new File("b.txt");
             File file3 = new File("c.txt");
-            URL url1 = new URL("https://www.cyberforum.ru/java-j2se/");
-            URL url2 = new URL("https://www.cyberforum.ru/java-j2se/");
+            URL url1 = new URL("http://www.pravex.com.ua/rus/services/personal/currency-converter");
+            URL url2 = new URL("http://www.pravex.com.ua/rus/services/personal/currency-converter");
             PrintWriter writer = new PrintWriter(file3);
-            writer.print(CompareSimbolsURL(url1,url2)+"111");
+            writer.print(CompareSimbolsURL(url1,url2));
             writer.close();
         }catch (IOException e){
             e.getStackTrace();
